691f6bf64ffb0152885f693303919a6242874c01
= <pablo.dorrio.vazquez@gmail.com>
2024-12-09 10:44:55 +0100

    feat: provisional styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,74 @@
 import React, { useState } from 'react';
+import '../styles/components/Auth.css'
 
 const Login = ({ changeView }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-    try {
-      const response = await fetch(`'http://localhost:80/eazypay-api/rest/user/${username}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': 'Basic ' + btoa(username + ':' + password), // Basic Auth
-        },
-      });
+        try {
+            const response = await fetch(`'http://localhost:80/eazypay-api/rest/user/${username}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Basic ' + btoa(username + ':' + password), // Basic Auth
+                },
+            });
 
-      if (response.ok) {
-        const data = await response.text();
+            if (response.ok) {
+                const data = await response.text();
 
-        // Save token and other info that API returns
-        localStorage.setItem('username', username);
-        changeView('dashboard');  // Change into Dashboard view
-      } else {
-        throw new Error('Login failed');
-      }
-    } catch (err) {
-      setError('Invalid username or password');
-    }
-  };
+                // Save token and other info that API returns
+                localStorage.setItem('username', username);
+                changeView('dashboard');  // Change into Dashboard view
+            } else {
+                throw new Error('Login failed');
+            }
+        } catch (err) {
+            setError('Invalid username or password');
+        }
+    };
 
-  return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    return (
+        <div className="register-section">
+            <div className="register-form-container">
+                {/* <h1>Login</h1> */}
+                <div className="form-icon">
+                    <img src="images/isotype.png" alt="icon" />
+                </div>
+                <form onSubmit={handleLogin}>
+                     <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                        <span className="error-message">Provisional error</span>
+                    </div>
+                     <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <span className="error-message">Provisional error</span>
+                    </div>
+                    {error && <p>{error}</p>}
+                    <button type="submit">Login</button>
+                </form>
+                <p>
+                    Don't have an account?{' '}
+                    <button onClick={() => changeView('register')}>Register</button>
+                </p>
+            </div>
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account?{' '}
-        <button onClick={() => changeView('register')}>Register</button>
-      </p>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,77 +1,86 @@
 import React, { useState } from 'react';
+import '../styles/components/Auth.css'
 
 const Register = ({ changeView }) => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
+    const handleRegister = async (e) => {
+        e.preventDefault();
 
-    const userData = { username, email, password };
+        const userData = { username, email, password };
 
-    try {
-      const response = await fetch('http://localhost:80/rest/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      });
+        try {
+            const response = await fetch('http://localhost:80/rest/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
 
-      if (response.ok) {
-        const data = await response.text();
-        changeView('login');  // Change into login view after register
-      } else {
-        const data = await response.json();
-        setError(data.errors.join(', ')); // Show validation errors
-      }
-    } catch (err) {
-      setError('Registration failed');
-    }
-  };
+            if (response.ok) {
+                const data = await response.text();
+                changeView('login');  // Change into login view after register
+            } else {
+                const data = await response.json();
+                setError(data.errors.join(', ')); // Show validation errors
+            }
+        } catch (err) {
+            setError('Registration failed');
+        }
+    };
 
-  return (
-    <div className="register-container">
-      <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    return (
+        <div className="register-section">
+            <div className="register-form-container">
+                {/* <h1>Register</h1> */}
+                <div className="form-icon">
+                    <img src="images/isotype.png" alt="icon" />
+                </div>
+                <form onSubmit={handleRegister}>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                        <span className="error-message">Provisional error</span>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <span className="error-message">Provisional error</span>
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <span className="error-message">Provisional error</span>
+                    </div>
+                    {error && <p>{error}</p>}
+                    <button type="submit">Register</button>
+                </form>
+                <p>
+                    Already have an account?{' '}
+                    <button onClick={() => changeView('login')}>Login</button>
+                </p>
+            </div>
         </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Register</button>
-      </form>
-      <p>
-        Already have an account?{' '}
-        <button onClick={() => changeView('login')}>Login</button>
-      </p>
-    </div>
-  );
+    );
 };
 
 export default Register;
