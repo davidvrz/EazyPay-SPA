b10d7e2863387c99891f580aca59a0c83b4b5b75
Pebb <pabloariasc2003@gmail.com>
2025-01-10 11:26:22 +0100

    fix: redirect into home by clicking app logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import '../styles/components/Navbar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ changeView }) => {
+    const currentUser = localStorage.getItem("username");
+
     const handleLogout = () => {
         localStorage.removeItem('username');
         window.location.href = '/';  // Redirect to login (or reload the page)
@@ -9,7 +12,12 @@ const Navbar = ({ changeView }) => {
 
     return (
         <nav className="navbar">
-            <img className="navbar-logo" src="/images/logo.png" alt="logo" />
+            {currentUser && (
+                <Link className="return-home" to="/home">
+                    <img className="navbar-logo" src="/images/logo.png" alt="logo" />
+                </Link>
+            )}
+
                 <ul className="navbar-links">
                     <li><button onClick={handleLogout}>Logout</button></li>
                     <li>prueba</li>
