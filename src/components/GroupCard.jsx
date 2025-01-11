4659af31596cb9017c5594bc40e2940831fa5c24
Pebb <pabloariasc2003@gmail.com>
2025-01-11 02:03:38 +0100

    feat: link to redirect into groups info

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -4,24 +4,26 @@ import "../styles/components/GroupCard.css";
 
 const GroupCard = ({ group, isAdmin, onDelete }) => {
   return (
-    <div className="group-card">
-      <div className="group-info">
-        <h3>{group.name}</h3>
-        {group.description && <p>{group.description}</p>}
+    <Link to={`/group/${group.id}`} className="group-info-btn">
+      <div className="group-card">
+        <div className="group-info">
+          <h3>{group.name}</h3>
+          {group.description && <p>{group.description}</p>}
+        </div>
+        <div className="group-actions">
+          {isAdmin && (
+            <>
+              <Link to={`/editgroup/${group.id}`} className="edit-btn">
+                Edit
+              </Link>
+              <button className="delete-btn" onClick={() => onDelete(group.id)}>
+                Delete
+              </button>
+            </>
+          )}
+        </div>
       </div>
-      <div className="group-actions">
-        {isAdmin && (
-          <>
-            <Link to={`/editgroup/${group.id}`} className="edit-btn">
-              Edit
-            </Link>
-            <button className="delete-btn" onClick={() => onDelete(group.id)}>
-              Delete
-            </button>
-          </>
-        )}
-      </div>
-    </div>
+    </Link>
   );
 };
 
