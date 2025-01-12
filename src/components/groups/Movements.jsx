0c1bc2d5deb574c01f7f6dc7968d8d57a95403b8
Pebb <pabloariasc2003@gmail.com>
2025-01-12 19:55:56 +0100

    fix: new suggested movements visualization

--- src/components/groups/Movements.jsx
+++ src/components/groups/Movements.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import "../../styles/components/Movements.css"; 
 import { useTranslation } from "react-i18next";
@@ -7,8 +7,10 @@ import { useTranslation } from "react-i18next";
 const Movements = () => {
   const { groupId } = useParams(); 
   const [movements, setMovements] = useState([]);
+  const [group, setGroup] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
   const {t} = useTranslation();
 
   
@@ -16,7 +18,10 @@ const Movements = () => {
     const fetchMovements = async () => {
       try {
         const response = await api.get(`/group/${groupId}/movements`);
+        const group_info = await api.get(`/group/${groupId}`);
+
         setMovements(response.data); 
+        setGroup(group_info.data);
       } catch (err) {
         setError(t('error-load-movements'));
       } finally {
@@ -36,21 +41,31 @@ const Movements = () => {
   }
 
   return (
-    <div className="movements-main">
-      <h1 className="movements-title">{t('form-suggested-movements')}</h1>
+    <div className="main">
+      <div className="top-icon">
+          <img src="/images/isotype.png" alt="Groups Icon" />
+      </div>
+
+      <h1 className="main-title">{t('form-suggested-movements')}: {group.name}</h1>
 
       {movements.length > 0 ? (
         <ul className="movements-list">
           {movements.map((movement, index) => (
             <li key={index} className="movement-item">
-              <strong>{movement.from}</strong> {t('movement_debts')} <strong>{movement.to}</strong>:
-              {movement.amount} €
+              <span class="movement-payer">{movement.from}</span>
+              <span class="movement-action">{t('movement_debts')}</span>
+              <span class="movement-receiver">{movement.to}</span>
+              <span class="movement-amount">{movement.amount} €</span>
             </li>
           ))}
         </ul>
       ) : (
         <p>{t('error-movements-info')}</p>
       )}
+
+      <div class="back-button">
+        <button onClick={() => navigate(`/group/${groupId}`)}>{t('back-button')}</button>
+      </div>
     </div>
   );
 };
